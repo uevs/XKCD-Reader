9dfaa84324d54e2f18c1e3ed988d4cda93b66572
uevs <36189306+uevs@users.noreply.github.com>
2022-04-12 14:51:08 +0200

    Initial Commit

---
+++ ShortcutChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShortcutChallenge
+//
+//  Created by leonardo on 12/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShortcutChallenge/ShortcutChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcutChallengeApp.swift
+//  ShortcutChallenge
+//
+//  Created by leonardo on 12/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ShortcutChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
